70f1ac9d3277b0860ae0adfeab85b67ae8af1b75
Eric Buckthal <ebuckthal@gmail.com>
2014-04-16 19:12:18 -0700

    expandability

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var tiles = $(".tile")
 
+$('.tile').append('<i class="fa fa-plus-square invisible expand"></i><i class="fa fa-minus-square invisible contract"></i>');
+
 $(".tile").draggable();
 
 $(".tile").on('mouseenter', function() {
@@ -8,13 +10,31 @@ $(".tile").on('mouseenter', function() {
       return;
    }
 
+   $('.tile').removeClass("active").find('.expand').addClass('invisible');
+   $(this).addClass('active');
+
+   $(this).find('.expand').removeClass("invisible");
+
    this.style.zIndex = tiles.length;
+
 
    $.each(tiles, function(k,v) {
       v.style.zIndex--;
    });
 
 })
+
+$('.tile .expand').on('click', function() {
+   $(this).parent().addClass('expanded');
+   $(this).parent().find('.expand').addClass('invisible');
+   $(this).parent().find('.contract').removeClass('invisible');
+});
+
+$('.tile .contract').on('click', function() {
+   $(this).parent().removeClass('expanded');
+   $(this).parent().find('.expand').removeClass('invisible');
+   $(this).parent().find('.contract').addClass('invisible');
+});
 
 for(var i = 0, tile, nextTop = 0, nextLeft = 0; tile = tiles[i++]; ) {
 
